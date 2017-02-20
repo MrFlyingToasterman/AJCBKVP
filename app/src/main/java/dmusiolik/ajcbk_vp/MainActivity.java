641bda9b8bbdf58b0a7d83b04f7dd3ac9aa244ec
Darius Musiolik <MrFlyingToasterman@yahoo.de>
2017-02-20 08:15:35 +0100

    added networkconnection test

--- app/src/main/java/dmusiolik/ajcbk_vp/MainActivity.java
+++ app/src/main/java/dmusiolik/ajcbk_vp/MainActivity.java
@@ -1,6 +1,8 @@
 package dmusiolik.ajcbk_vp;
 
 import android.app.AlertDialog;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,6 +10,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.HttpAuthHandler;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
@@ -25,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void reload(View view) {
         browse();
-        displaytoast("Neues Datenblatt geladen.");
+        displaytoast("Neues Datenblatt angefordert.");
     }
 
     public void browse() {
@@ -36,6 +39,16 @@ public class MainActivity extends AppCompatActivity {
         browser.getSettings().setSupportMultipleWindows(false);
         browser.getSettings().setSupportZoom(false);
         browser.setVerticalScrollBarEnabled(false);
+        browser.getSettings().setAppCachePath( getApplicationContext().getCacheDir().getAbsolutePath() );
+        browser.getSettings().setAllowFileAccess( true );
+        browser.getSettings().setAppCacheEnabled( true );
+        browser.getSettings().setCacheMode( WebSettings.LOAD_DEFAULT );
+
+        if ( !isNetworkAvailable() ) { // loading offline
+            //browser.getSettings().setCacheMode( WebSettings.LOAD_CACHE_ONLY );
+            displaytoast("Zeige Offline version an!");
+        }
+
         browser.loadUrl("http://ajc-bk.dyndns.org:8008/Vertretung-Online/");
         //Parse htmlcode -> Sehr viel Arbeit mit der Mommentanet API -- Vielleicht mal in der Zukunft
 
@@ -55,7 +68,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //webview.setWebViewClient(new HelloWebViewClient());
     }
 
     @Override
@@ -92,6 +104,13 @@ public class MainActivity extends AppCompatActivity {
     public void displaytoast(String msg) {
         Toast.makeText(MainActivity.this,
                 msg, Toast.LENGTH_SHORT).show();
+    }
+
+    //Check if there is a connection to the Internet
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService( CONNECTIVITY_SERVICE );
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 
 }
